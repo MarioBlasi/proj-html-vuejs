b6670b404bb90cbf37c55dd48f6879a300cc4f74
MarioBlasi <mblasi509@gmail.com>
2023-04-27 17:24:34 +0100

    v-for/v-if footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -4,7 +4,35 @@ import { store } from "../store";
 export default {
   name: " PageFooter",
   data() {
-    return { store };
+    return {
+      textfooter: [
+        {
+          adress: "Address",
+          date: "382 NE191st St#87394 Miami, FL33179-3899",
+          tel: "+1(305) 547-9909(9am-5pm EST, Monday-Friday)",
+          email: "support@maxcoach.com",
+        },
+      ],
+      // social: [
+      //   {
+      //     iconF: "Address",
+      //     current_page: true,
+      //   },
+      //   {
+      //     iconT: "Explore",
+      //     current_page: false,
+      //   },
+      //   {
+      //     iconI: "Information",
+      //     current_page: false,
+      //   },
+      //   {
+      //     iconIn: "Information",
+      //     current_page: false,
+      //   },
+      // ],
+      store,
+    };
   },
 
   methods: {},
@@ -13,13 +41,17 @@ export default {
 
 <template>
   <footer>
-    <div class="container text-center">
-      <h1>FOOTER</h1>
-      <div class="row">
-        <div class="col-6">.col-6</div>
-        <div class="col-2">.col-6</div>
-        <div class="col-2">.col-6</div>
-        <div class="col-2">.col-6</div>
+    <div class="container mt-5 pt-5">
+      <div class="row d-flex" v-for="(item, index) in textfooter" :key="index">
+        <div class="col-6" v-if="item.date">
+          <h5>{{ item.adress }}</h5>
+          <p>{{ item.date }}</p>
+          <p>{{ item.tel }}</p>
+          <p>{{ item.email }}</p>
+        </div>
+        <div class="col-2" v-if="item.date"></div>
+        <div class="col-2" v-if="item.text"></div>
+        <div class="col-2" v-if="item.text"></div>
       </div>
     </div>
   </footer>
@@ -27,6 +59,7 @@ export default {
 
 <style lang="scss">
 footer {
-  background-color: rgb(241, 243, 246);
+  height: 500px;
+  background-color: rgb(241, 243, 242);
 }
 </style>
